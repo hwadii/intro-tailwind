4fad7ef87991ff8cb0091d71c485139b2877f5ee
hwadii <wadii.hajji@etu.univ-amu.fr>
2020-10-03 15:30:12 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            ident: 'postcss',
+            plugins: [
+              require('tailwindcss'),
+              require('autoprefixer'),
+              require('postcss-import')
+            ],
+          },
+        }
+      }
+    ]
+  }
+};
